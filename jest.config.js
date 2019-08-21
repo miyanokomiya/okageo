4b401d641d8da4b19fbf4bfce8f72b737a5e8b5b
miyanokomiya <tomoya.hstexze24632@gmail.com>
2019-08-21 22:16:38 +0900

    fix lint

--- jest.config.js
+++ jest.config.js
@@ -5,14 +5,7 @@ module.exports = {
       babelConfig: true
     }
   },
-  moduleFileExtensions: [
-    'ts',
-    'tsx',
-    'js',
-    'jsx',
-    'json',
-    'node'
-  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
