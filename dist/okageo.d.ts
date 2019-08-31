283fd8e1335d1fc5e722741428f54b30bffcf7c1
miyanokomiya <tomoya.hstexze24632@gmail.com>
2019-08-31 16:22:13 +0900

    モデル型をexport

--- dist/okageo.d.ts
+++ dist/okageo.d.ts
@@ -1,5 +1,6 @@
 import * as geo from './geo';
 import * as svg from './svg';
+export * from './types';
 declare const _default: {
     geo: typeof geo;
     svg: typeof svg;
--- src/okageo.ts
+++ src/okageo.ts
@@ -1,6 +1,7 @@
 import * as geo from './geo'
 import * as svg from './svg'
 
+export * from './types'
 export default {
   geo,
   svg
