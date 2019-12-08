6a7c2cb17d86452f5ad9139330dcc6b59d20855f
miyanokomiya <tomoya.hstexze24632@gmail.com>
2019-12-08 11:26:20 +0900

    rm dist

--- dist/okageo.d.ts
+++
@@ -1,8 +0,0 @@
-import * as geo from './geo';
-import * as svg from './svg';
-export * from './types';
-declare const _default: {
-    geo: typeof geo;
-    svg: typeof svg;
-};
-export default _default;
