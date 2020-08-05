f159e90d96ca82f9b291fe4b2b0a35dfd4881a16
miyanokomiya <tomoya.hstexze24632@gmail.com>
2020-08-05 21:53:51 +0900

    fix

--- src/drawing.ts
+++ src/drawing.ts
@@ -4,15 +4,13 @@ import { IVec2, IRectangle } from './types'
  * 矩形を左辺からリサイズ
  * @param org 元の矩形
  * @param diffX 変更値
- * @return サイズ変更後の矩形プロパティ
+ * @return サイズ変更後の矩形
  */
-export function resizeRectByLeft(
-  org: IRectangle,
-  diffX: number
-): Partial<IRectangle> {
+export function resizeRectByLeft(org: IRectangle, diffX: number): IRectangle {
   const x = org.x + diffX
   const width = org.width - diffX
   return {
+    ...org,
     x: Math.min(x, x + width),
     width: Math.abs(width),
   }
@@ -22,18 +20,17 @@ export function resizeRectByLeft(
  * 矩形を右辺からリサイズ
  * @param org 元の矩形
  * @param diffX 変更値
- * @return サイズ変更後の矩形プロパティ
+ * @return サイズ変更後の矩形
  */
-export function resizeRectByRight(
-  org: IRectangle,
-  diffX: number
-): Partial<IRectangle> {
+export function resizeRectByRight(org: IRectangle, diffX: number): IRectangle {
   const width = org.width + diffX
   return width >= 0
     ? {
+        ...org,
         width,
       }
     : {
+        ...org,
         x: org.x + width,
         width: -width,
       }
@@ -43,15 +40,13 @@ export function resizeRectByRight(
  * 矩形を上辺からリサイズ
  * @param org 元の矩形
  * @param diffY 変更値
- * @return サイズ変更後の矩形プロパティ
+ * @return サイズ変更後の矩形
  */
-export function resizeRectByTop(
-  org: IRectangle,
-  diffY: number
-): Partial<IRectangle> {
+export function resizeRectByTop(org: IRectangle, diffY: number): IRectangle {
   const y = org.y + diffY
   const height = org.height - diffY
   return {
+    ...org,
     y: Math.min(y, y + height),
     height: Math.abs(height),
   }
@@ -61,18 +56,17 @@ export function resizeRectByTop(
  * 矩形を下辺からリサイズ
  * @param org 元の矩形
  * @param diffY 変更値
- * @return サイズ変更後の矩形プロパティ
+ * @return サイズ変更後の矩形
  */
-export function resizeRectByBottom(
-  org: IRectangle,
-  diffY: number
-): Partial<IRectangle> {
+export function resizeRectByBottom(org: IRectangle, diffY: number): IRectangle {
   const height = org.height + diffY
   return height >= 0
     ? {
+        ...org,
         height,
       }
     : {
+        ...org,
         y: org.y + height,
         height: -height,
       }
@@ -82,62 +76,44 @@ export function resizeRectByBottom(
  * 矩形を左上頂点からリサイズ
  * @param org 元の矩形
  * @param diff 変更ベクトル
- * @return サイズ変更後の矩形プロパティ
+ * @return サイズ変更後の矩形
  */
-export function resizeRectByLeftTop(
-  org: IRectangle,
-  diff: IVec2
-): Partial<IRectangle> {
-  return {
-    ...resizeRectByLeft(org, diff.x),
-    ...resizeRectByTop(org, diff.y),
-  }
+export function resizeRectByLeftTop(org: IRectangle, diff: IVec2): IRectangle {
+  return resizeRectByTop(resizeRectByLeft(org, diff.x), diff.y)
 }
 
 /**
  * 矩形を右上頂点からリサイズ
  * @param org 元の矩形
  * @param diff 変更ベクトル
- * @return サイズ変更後の矩形プロパティ
+ * @return サイズ変更後の矩形
  */
-export function resizeRectByRightTop(
-  org: IRectangle,
-  diff: IVec2
-): Partial<IRectangle> {
-  return {
-    ...resizeRectByRight(org, diff.x),
-    ...resizeRectByTop(org, diff.y),
-  }
+export function resizeRectByRightTop(org: IRectangle, diff: IVec2): IRectangle {
+  return resizeRectByTop(resizeRectByRight(org, diff.x), diff.y)
 }
 
 /**
  * 矩形を右下頂点からリサイズ
  * @param org 元の矩形
  * @param diff 変更ベクトル
- * @return サイズ変更後の矩形プロパティ
+ * @return サイズ変更後の矩形
  */
 export function resizeRectByRightBottom(
   org: IRectangle,
   diff: IVec2
-): Partial<IRectangle> {
-  return {
-    ...resizeRectByRight(org, diff.x),
-    ...resizeRectByBottom(org, diff.y),
-  }
+): IRectangle {
+  return resizeRectByBottom(resizeRectByRight(org, diff.x), diff.y)
 }
 
 /**
  * 矩形を左下頂点からリサイズ
  * @param org 元の矩形
  * @param diff 変更ベクトル
- * @return サイズ変更後の矩形プロパティ
+ * @return サイズ変更後の矩形
  */
 export function resizeRectByLeftBottom(
   org: IRectangle,
   diff: IVec2
-): Partial<IRectangle> {
-  return {
-    ...resizeRectByLeft(org, diff.x),
-    ...resizeRectByBottom(org, diff.y),
-  }
+): IRectangle {
+  return resizeRectByBottom(resizeRectByLeft(org, diff.x), diff.y)
 }
--- test/drawing.test.ts
+++ test/drawing.test.ts
@@ -5,13 +5,25 @@ describe('resizeByLeft', () => {
 
   describe('widthが正になる場合', () => {
     it('xの変化に合わせてwidthが変動', () => {
-      expect(drawing.resizeRectByLeft(rec, 3)).toEqual({ x: 4, width: 7 })
-      expect(drawing.resizeRectByLeft(rec, 10)).toEqual({ x: 11, width: 0 })
+      expect(drawing.resizeRectByLeft(rec, 3)).toEqual({
+        ...rec,
+        x: 4,
+        width: 7,
+      })
+      expect(drawing.resizeRectByLeft(rec, 10)).toEqual({
+        ...rec,
+        x: 11,
+        width: 0,
+      })
     })
   })
   describe('widthが負になる場合', () => {
     it('widthが正になるよう形を保つ', () => {
-      expect(drawing.resizeRectByLeft(rec, 11)).toEqual({ x: 11, width: 1 })
+      expect(drawing.resizeRectByLeft(rec, 11)).toEqual({
+        ...rec,
+        x: 11,
+        width: 1,
+      })
     })
   })
 })
@@ -21,13 +33,17 @@ describe('resizeByRight', () => {
 
   describe('widthが正になる場合', () => {
     it('widthが変動', () => {
-      expect(drawing.resizeRectByRight(rec, 3)).toEqual({ width: 13 })
-      expect(drawing.resizeRectByRight(rec, -3)).toEqual({ width: 7 })
+      expect(drawing.resizeRectByRight(rec, 3)).toEqual({ ...rec, width: 13 })
+      expect(drawing.resizeRectByRight(rec, -3)).toEqual({ ...rec, width: 7 })
     })
   })
   describe('widthが負になる場合', () => {
     it('widthが正になるよう形を保つ', () => {
-      expect(drawing.resizeRectByRight(rec, -11)).toEqual({ x: 0, width: 1 })
+      expect(drawing.resizeRectByRight(rec, -11)).toEqual({
+        ...rec,
+        x: 0,
+        width: 1,
+      })
     })
   })
 })
@@ -37,13 +53,25 @@ describe('resizeByTop', () => {
 
   describe('heightが正になる場合', () => {
     it('yの変化に合わせてheightが変動', () => {
-      expect(drawing.resizeRectByTop(rec, 3)).toEqual({ y: 5, height: 17 })
-      expect(drawing.resizeRectByTop(rec, 20)).toEqual({ y: 22, height: 0 })
+      expect(drawing.resizeRectByTop(rec, 3)).toEqual({
+        ...rec,
+        y: 5,
+        height: 17,
+      })
+      expect(drawing.resizeRectByTop(rec, 20)).toEqual({
+        ...rec,
+        y: 22,
+        height: 0,
+      })
     })
   })
   describe('heightが負になる場合', () => {
     it('heightが正になるよう形を保つ', () => {
-      expect(drawing.resizeRectByTop(rec, 21)).toEqual({ y: 22, height: 1 })
+      expect(drawing.resizeRectByTop(rec, 21)).toEqual({
+        ...rec,
+        y: 22,
+        height: 1,
+      })
     })
   })
 })
@@ -53,13 +81,20 @@ describe('resizeByBottom', () => {
 
   describe('heightが正になる場合', () => {
     it('heightが変動', () => {
-      expect(drawing.resizeRectByBottom(rec, 3)).toEqual({ height: 23 })
-      expect(drawing.resizeRectByBottom(rec, -20)).toEqual({ height: 0 })
+      expect(drawing.resizeRectByBottom(rec, 3)).toEqual({ ...rec, height: 23 })
+      expect(drawing.resizeRectByBottom(rec, -20)).toEqual({
+        ...rec,
+        height: 0,
+      })
     })
   })
   describe('heightが負になる場合', () => {
     it('heightが正になるよう形を保つ', () => {
-      expect(drawing.resizeRectByBottom(rec, -21)).toEqual({ y: 1, height: 1 })
+      expect(drawing.resizeRectByBottom(rec, -21)).toEqual({
+        ...rec,
+        y: 1,
+        height: 1,
+      })
     })
   })
 })
@@ -69,12 +104,14 @@ describe('resizeByLeftTop', () => {
 
   it('左上頂点からリサイズ', () => {
     expect(drawing.resizeRectByLeftTop(rec, { x: 3, y: 4 })).toEqual({
+      ...rec,
       x: 4,
       width: 7,
       y: 6,
       height: 16,
     })
     expect(drawing.resizeRectByLeftTop(rec, { x: 11, y: 21 })).toEqual({
+      ...rec,
       x: 11,
       width: 1,
       y: 22,
@@ -88,11 +125,13 @@ describe('resizeByRightTop', () => {
 
   it('右上頂点からリサイズ', () => {
     expect(drawing.resizeRectByRightTop(rec, { x: 3, y: -4 })).toEqual({
+      ...rec,
       width: 13,
       y: -2,
       height: 24,
     })
     expect(drawing.resizeRectByRightTop(rec, { x: -11, y: 21 })).toEqual({
+      ...rec,
       x: 0,
       width: 1,
       y: 22,
@@ -106,10 +145,12 @@ describe('resizeByRightBottom', () => {
 
   it('右下頂点からリサイズ', () => {
     expect(drawing.resizeRectByRightBottom(rec, { x: 3, y: 4 })).toEqual({
+      ...rec,
       width: 13,
       height: 24,
     })
     expect(drawing.resizeRectByRightBottom(rec, { x: -11, y: -21 })).toEqual({
+      ...rec,
       x: 0,
       width: 1,
       y: 1,
@@ -123,11 +164,13 @@ describe('resizeByLeftBottom', () => {
 
   it('左下頂点からリサイズ', () => {
     expect(drawing.resizeRectByLeftBottom(rec, { x: -3, y: 4 })).toEqual({
+      ...rec,
       x: -2,
       width: 13,
       height: 24,
     })
     expect(drawing.resizeRectByLeftBottom(rec, { x: 11, y: -21 })).toEqual({
+      ...rec,
       x: 11,
       width: 1,
       y: 1,
