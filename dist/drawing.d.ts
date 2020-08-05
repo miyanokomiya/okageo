ddea8395987f53fdb6530b6d5b1b95f2100fbb1e
miyanokomiya <tomoya.hstexze24632@gmail.com>
2020-08-05 21:56:27 +0900

    build

--- dist/drawing.d.ts
+++ dist/drawing.d.ts
@@ -3,55 +3,55 @@ import { IVec2, IRectangle } from './types';
  * 矩形を左辺からリサイズ
  * @param org 元の矩形
  * @param diffX 変更値
- * @return サイズ変更後の矩形プロパティ
+ * @return サイズ変更後の矩形
  */
-export declare function resizeRectByLeft(org: IRectangle, diffX: number): Partial<IRectangle>;
+export declare function resizeRectByLeft(org: IRectangle, diffX: number): IRectangle;
 /**
  * 矩形を右辺からリサイズ
  * @param org 元の矩形
  * @param diffX 変更値
- * @return サイズ変更後の矩形プロパティ
+ * @return サイズ変更後の矩形
  */
-export declare function resizeRectByRight(org: IRectangle, diffX: number): Partial<IRectangle>;
+export declare function resizeRectByRight(org: IRectangle, diffX: number): IRectangle;
 /**
  * 矩形を上辺からリサイズ
  * @param org 元の矩形
  * @param diffY 変更値
- * @return サイズ変更後の矩形プロパティ
+ * @return サイズ変更後の矩形
  */
-export declare function resizeRectByTop(org: IRectangle, diffY: number): Partial<IRectangle>;
+export declare function resizeRectByTop(org: IRectangle, diffY: number): IRectangle;
 /**
  * 矩形を下辺からリサイズ
  * @param org 元の矩形
  * @param diffY 変更値
- * @return サイズ変更後の矩形プロパティ
+ * @return サイズ変更後の矩形
  */
-export declare function resizeRectByBottom(org: IRectangle, diffY: number): Partial<IRectangle>;
+export declare function resizeRectByBottom(org: IRectangle, diffY: number): IRectangle;
 /**
  * 矩形を左上頂点からリサイズ
  * @param org 元の矩形
  * @param diff 変更ベクトル
- * @return サイズ変更後の矩形プロパティ
+ * @return サイズ変更後の矩形
  */
-export declare function resizeRectByLeftTop(org: IRectangle, diff: IVec2): Partial<IRectangle>;
+export declare function resizeRectByLeftTop(org: IRectangle, diff: IVec2): IRectangle;
 /**
  * 矩形を右上頂点からリサイズ
  * @param org 元の矩形
  * @param diff 変更ベクトル
- * @return サイズ変更後の矩形プロパティ
+ * @return サイズ変更後の矩形
  */
-export declare function resizeRectByRightTop(org: IRectangle, diff: IVec2): Partial<IRectangle>;
+export declare function resizeRectByRightTop(org: IRectangle, diff: IVec2): IRectangle;
 /**
  * 矩形を右下頂点からリサイズ
  * @param org 元の矩形
  * @param diff 変更ベクトル
- * @return サイズ変更後の矩形プロパティ
+ * @return サイズ変更後の矩形
  */
-export declare function resizeRectByRightBottom(org: IRectangle, diff: IVec2): Partial<IRectangle>;
+export declare function resizeRectByRightBottom(org: IRectangle, diff: IVec2): IRectangle;
 /**
  * 矩形を左下頂点からリサイズ
  * @param org 元の矩形
  * @param diff 変更ベクトル
- * @return サイズ変更後の矩形プロパティ
+ * @return サイズ変更後の矩形
  */
-export declare function resizeRectByLeftBottom(org: IRectangle, diff: IVec2): Partial<IRectangle>;
+export declare function resizeRectByLeftBottom(org: IRectangle, diff: IVec2): IRectangle;
