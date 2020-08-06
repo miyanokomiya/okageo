76b0c80daec4d32d0f405c352b9511eaed9d34a8
miyanokomiya <tomoya.hstexze24632@gmail.com>
2020-08-07 00:30:40 +0900

    build

--- dist/drawing.d.ts
+++ dist/drawing.d.ts
@@ -31,27 +31,31 @@ export declare function resizeRectByBottom(org: IRectangle, diffY: number): IRec
  * 矩形を左上頂点からリサイズ
  * @param org 元の矩形
  * @param diff 変更ベクトル
+ * @param keepAspectRate アスペクト比維持
  * @return サイズ変更後の矩形
  */
-export declare function resizeRectByLeftTop(org: IRectangle, diff: IVec2): IRectangle;
+export declare function resizeRectByLeftTop(org: IRectangle, diff: IVec2, keepAspectRate?: boolean): IRectangle;
 /**
  * 矩形を右上頂点からリサイズ
  * @param org 元の矩形
  * @param diff 変更ベクトル
+ * @param keepAspectRate アスペクト比維持
  * @return サイズ変更後の矩形
  */
-export declare function resizeRectByRightTop(org: IRectangle, diff: IVec2): IRectangle;
+export declare function resizeRectByRightTop(org: IRectangle, diff: IVec2, keepAspectRate?: boolean): IRectangle;
 /**
  * 矩形を右下頂点からリサイズ
  * @param org 元の矩形
  * @param diff 変更ベクトル
+ * @param keepAspectRate アスペクト比維持
  * @return サイズ変更後の矩形
  */
-export declare function resizeRectByRightBottom(org: IRectangle, diff: IVec2): IRectangle;
+export declare function resizeRectByRightBottom(org: IRectangle, diff: IVec2, keepAspectRate?: boolean): IRectangle;
 /**
  * 矩形を左下頂点からリサイズ
  * @param org 元の矩形
  * @param diff 変更ベクトル
+ * @param keepAspectRate アスペクト比維持
  * @return サイズ変更後の矩形
  */
-export declare function resizeRectByLeftBottom(org: IRectangle, diff: IVec2): IRectangle;
+export declare function resizeRectByLeftBottom(org: IRectangle, diff: IVec2, keepAspectRate?: boolean): IRectangle;
